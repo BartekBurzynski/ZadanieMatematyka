3abc37733577aad097ff53178ab77a13ea042fb2
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-05-09 19:11:37 +0200

    Stworzenie kalkulatora

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    int isEven (int a){
+
+
+        boolean isEven = a %2 == 0;
+            return a;
+
+    }
+
+    int isOdd (int b){
+        boolean osOdd = b %2 !=0;
+            return b;
+    }
+
+    double circleField (double c){
+        double circleField = 3.14*c*c;
+        return c;
+    }
+
+    int power ( int d ){
+        int power = d*d;
+        return d*d;
+    }
+}
+
---
+++ src/CalculatorTest.java
@@ -0,0 +1,10 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.isEven(4);
+        calc.isOdd(3);
+        calc.circleField(5);
+        calc.power(7);
+
+    }
+}
